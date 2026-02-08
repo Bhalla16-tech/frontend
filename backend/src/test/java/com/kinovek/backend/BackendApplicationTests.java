8488121d4e3d1e96293631f80b849183c6dbd063
Vadla Srikanth <vadlasrikanth2000@gmail.com>
2026-02-08 19:25:34 +0530

    Initial project setup with Spring Boot 3.2.3 and Java 17

---
+++ backend/src/test/java/com/kinovek/backend/BackendApplicationTests.java
@@ -0,0 +1,12 @@
+﻿package com.kinovek.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
